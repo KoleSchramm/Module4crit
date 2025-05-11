b901319e706d32a620d3e3b8cdb4395be7da2af3
KoleSchramm <kole.schramm@csuglobal.edu>
2025-05-11 02:34:02 -0500

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        double average = 0;
+        double minimum = 0;
+        double maximum = 0;
+        double input = 0;
 
+        Scanner scan = new Scanner(System.in);
+
+        for(int i = 1; i<11; i++){
+            System.out.println("Input grade " + i + ":");
+            input = scan.nextDouble();
+            average += input;
+            if(i == 1){
+                minimum = input;
+            }
+            else if(input<minimum){
+                minimum = input;
+            }
+            if(i == 1){
+                maximum = input;
+            }
+            else if(input>maximum){
+                maximum = input;
+            }
+        }
+
+        average = average/10;
+
+        System.out.println("Average: " + average);
+        System.out.println("Minimum: " + minimum);
+        System.out.println("Maximum: " + maximum);
     }
 }
